b5c4d44a64e549e1d19fda943dffa82dd4b254a4
Kevin-Wei-sudo <60225117+Kevin-Wei-sudo@users.noreply.github.com>
2026-07-26 08:32:13 +0800

    claude-scope 0.3.1

--- Casks/claude-scope.rb
+++ Casks/claude-scope.rb
@@ -1,6 +1,6 @@
 cask "claude-scope" do
-  version "0.3.0"
-  sha256 "a4adb360f286751cadca8417c33c5a281fad22dc3b1499f46f5739846790e82e"
+  version "0.3.1"
+  sha256 "9c98bacf01959c8b6b7af327353eaf4c240c275ad54b7041cc7d337409e7c439"
 
   url "https://github.com/Kevin-Wei-sudo/claude-scope/releases/download/v#{version}/ClaudeScope.dmg"
   name "ClaudeScope"
